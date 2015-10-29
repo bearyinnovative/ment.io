b8a75a929bf6a5e90de79adf155fc38f5ac35547
loddit <loddit@gmail.com>
2015-10-30 00:39:05 +0800

    fix shortcut bug

--- dist/mentio.js
+++ dist/mentio.js
@@ -221,7 +221,7 @@ angular.module('mentio', [])
                                 });
                             }
 
-                            if (event.which === 40 ||
+                            if ((event.which === 40 && !event.shiftKey) ||
                                 (event.which === 78 && event.ctrlKey)) {
                                 event.preventDefault();
                                 activeMenuScope.$apply(function () {
@@ -229,7 +229,7 @@ angular.module('mentio', [])
                                 });
                             }
 
-                            if (event.which === 38 ||
+                            if ((event.which === 38 && !event.shiftKey) ||
                                 (event.which === 80 && event.ctrlKey)) {
                                 event.preventDefault();
                                 activeMenuScope.$apply(function () {
